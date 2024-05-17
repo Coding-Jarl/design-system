31e4bc3cb0d6aa420c9596b88b9e3a56a7418ac6
Forthtilliath <forth@live.fr>
2024-05-18 00:39:09 +0200

    chore: add Theme inside decorator
    - add Theme inside decorator
    - add addCode function
    ```jsx
    <Story
    	name="Large With Template"
    	args={{ label: 'Button', size: 'large' }}
    	parameters={{ docs: { source: { code: '<Button label="Button" backgroundColor="green" />' } } }}
    />
    // to
    <Story
    	name="Large With Template"
    	args={{ label: 'Button', size: 'large' }}
    	parameters={addCode('<Button label="Button" size="large" />')}
    />
    ```

--- v0.1/src/stories/atoms/Button.stories.svelte
+++ v0.1/src/stories/atoms/Button.stories.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" context="module">
 	import type { Meta } from '@storybook/svelte';
+	import TemplateWithTheme from '../templates/TemplateWithTheme.svelte';
 	import Button from '../acme/Button.svelte';
 
 	export const meta = {
@@ -8,37 +9,71 @@
 		tags: ['autodocs'],
 		argTypes: {
 			label: {
-				control: { type: 'text' }
+				control: 'text'
 			},
 			primary: {
-				control: { type: 'boolean' }
+				control: 'boolean',
+				defaultValue: false
 			},
 			backgroundColor: { control: 'color' },
 			size: {
-				control: { type: 'select' },
+				control: 'select',
 				options: ['small', 'medium', 'large']
 			}
-		}
+		},
+		parameters: {
+			backgrounds: {
+				default: 'dark',
+
+				values: [
+					{ name: 'light', value: '#f2f2f2' },
+					{ name: 'dark', value: '#1d1e22' }
+				]
+			},
+
+			layout: 'fullscreen'
+		},
+
+		decorators: [
+			(_story, ctx) => ({
+				Component: TemplateWithTheme,
+				props: ctx.args
+			})
+		]
 	} satisfies Meta<Button>;
 </script>
 
 <script lang="ts">
 	import { Story, Template } from '@storybook/addon-svelte-csf';
-	import TemplateWithTheme from '../templates/TemplateWithTheme.svelte';
+	import type { Parameters } from '@storybook/svelte';
+
+	function addCode(code: string, parameters: Parameters = {}): Parameters {
+		return Object.assign({}, parameters, { docs: { source: { code } } });
+	}
 </script>
 
 <Template let:args>
-	<TemplateWithTheme>
-		<Button {...args} />
-	</TemplateWithTheme>
+	<Button {...args} />
 </Template>
 
-<Story name="Isolation">
+<Story name="Default" args={{ label: 'Button' }} />
+
+<Story name="Primary">
 	<Button primary label="Button" />
 </Story>
 
+<Story name="Large">
+	<Button label="Button" size="large" />
+</Story>
+
 <Story
-	name="Base"
-	args={{ label: 'Button', primary: true }}
-	parameters={{ docs: { source: { code: '<Button variant="secondary" />' } } }}
+	name="Large With Template"
+	args={{ label: 'Button', size: 'large' }}
+	parameters={addCode(`<Button label="Button" size="large" />`)}
+/>
+
+<Story
+	name="Background Color With Template"
+	args={{ label: 'Button', backgroundColor: 'green' }}
+	parameters={addCode(`<Button label="Button" backgroundColor="green" />`)}
 />
