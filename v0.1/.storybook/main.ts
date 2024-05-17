459b1d9a6a8e57503fac05d7d2a118b1b8eb89a0
Forthtilliath <forth@live.fr>
2024-05-17 17:57:03 +0200

    build: upgrade packages

--- v0.1/.storybook/main.ts
+++ v0.1/.storybook/main.ts
@@ -3,19 +3,17 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)'],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions',
-
-		'@storybook/addon-svelte-csf'
-	],
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook',
+        '@storybook/addon-interactions',
+        '@storybook/addon-svelte-csf',
+        '@storybook/addon-mdx-gfm'
+    ],
 	framework: {
 		name: '@storybook/sveltekit',
 		options: {}
 	},
-	docs: {
-		autodocs: 'tag'
-	}
+	docs: {}
 };
 export default config;
